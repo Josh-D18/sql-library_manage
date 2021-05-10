105ca2bdc26313e7e97c667a1d31e32dd18337a9
unknown <jjdate@live.com>
2021-05-10 18:53:27 -0400

    added more views. working on part 8 and 9

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', async function(req, res, next) {
 
 router.get('/books', async (req, res, next)=>{
   const books = await Book.findAll();
-  res.render('index', { books })
+  res.render('index', { books : books })
 })
 
 router.get('/books/new', async (req, res, next)=>{
@@ -18,19 +18,31 @@ router.get('/books/new', async (req, res, next)=>{
 })
 
 router.post('/books/new', async (req, res, next)=>{
-  
+  const book = await Book.create(req.body);
+  res.redirect('/books/' + book.id);
 })
 
 router.get('/books/:id', async (req, res, next)=>{
+  const book = await Book.findByPk(req.params.id);
+  res.render("book-detail", { book : book });
+})
 
+// Get Edit Form
+router.get('/books/:id/edit', async (req, res, next)=>{
+  const book = await Book.findByPk(req.params.id);
+  res.render("update-book", { book : book });
 })
 
 router.post('/books/:id', async (req, res, next)=>{
-
+  const book = await Book.findByPk(req.params.id);
+  await book.update(req.body);
+  res.redirect('/')
 })
 
 router.post('/books/:id/delete', async (req, res, next)=>{
-
+  const book = await Book.findByPk(req.params.id);
+  book.destroy();
+  res.redirect('/');
 })
 
 module.exports = router;
