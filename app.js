f20146bd09fd092985bf71714f86b63dc6ffe77a
unknown <jjdate@live.com>
2021-05-11 20:32:51 -0400

    working on part 11

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -34,7 +34,6 @@ app.use((err, req, res, next) => {
   const error = new Error();
   error.status = 404;
   error.message = "Sorry Page Not Found!";
-  if (err){}
   res.render('page_not_found', { error })
 })
 
